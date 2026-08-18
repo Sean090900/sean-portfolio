969670e8dd1efd20a1d6302f81f09a6790927734
Sean Dickson <seandickson@Seans-MacBook-Pro-2.local>
2026-08-18 14:46:39 -0700

    make config edits for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sean-portfolio/',
+  // base: '/sean-portfolio/',
+  base: '/',
   plugins: [react()],
 })
